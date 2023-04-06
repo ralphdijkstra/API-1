e2def25042903c2af218ba166545c0cc20d37273
ps229592 <93653466+ps229592@users.noreply.github.com>
2023-04-06 13:19:46 +0200

    fixed double image

--- the-trailers/src/Components/Client/HeroSection.jsx
+++ the-trailers/src/Components/Client/HeroSection.jsx
@@ -14,7 +14,6 @@ export default function HeroSection() {
           id="fadebg"
           className="transition-all duration-700 ease-in-out bg-gradient-to-b from-[#010414]/20 via-[#010414]/20 to-[#010414]/100 h-full bg-no-repeat flex w-full"
         ></div>
-        <img src={placeholder} alt="" />
       </div>
     </div>
   );
